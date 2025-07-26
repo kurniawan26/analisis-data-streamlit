3dc17bef043a47fcd1d5f409f3a5eaa3c987a04f
Fauzi Kurniawan <71652809+kurniawan26@users.noreply.github.com>
2025-07-26 21:21:38 +0700

    refactor: rename the label for hari libur

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import streamlit as st
 
 import pages.cluster as cluster
 import pages.holiday as holiday
+import pages.weather as weather
 import utils.data_formatter as utils
 
 @st.cache_data
@@ -16,12 +17,15 @@ all_df = load_data()
 holiday_data = utils.create_by_holiday(all_df)
 season_data = utils.create_by_season(all_df)
 
-menu = st.sidebar.radio("Pilih Halaman", ["Overview","Tren Cluster"])
+menu = st.sidebar.radio("Pilih Halaman", ["Hari libur", "Cuaca","Tren Cluster"])
 
 st.title("Analisis Penyewaan Sepeda Harian")
 
-if menu == "Overview":
+if menu == "Hari libur":
     holiday.show(all_df, st)
 
-if menu == "Tren Cluster":
+elif menu == "Cuaca":
+    weather.show(all_df, st)
+
+elif menu == "Tren Cluster":
     cluster.show(all_df, st)
